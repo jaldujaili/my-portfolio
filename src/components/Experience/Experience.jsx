5de5461ede08bd7d626daa7b1719e4b73e6f411e
jaldujaili <jaldujaili@ucdavis.edu>
2024-02-01 00:38:55 -0800

    update styles

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -9,17 +9,19 @@ import { GoRuby } from "react-icons/go";
 export const Experience = () => {
   return (
     <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Experience</h2>
-      <div className={styles.content}>
-        <div className={styles.skills}>
-          <FaJava className={styles.skill}/>
-          <FaJs className={styles.skill}/>
-          <FaPython className={styles.skill}/>
-          <GoRuby className={styles.skill}/>
-          <FaReact className={styles.skill}/>
-          <FaSwift className={styles.skill}/>
-          <FaHtml5 className={styles.skill}/>
-          <FaCss3 className={styles.skill}/>
+      <div className={styles.innerContainer} >
+       <h2 className={styles.title}>Experience</h2>
+        <div className={styles.content}>
+          <div className={styles.skills}>
+            <FaJava className={styles.skill}/>
+            <FaJs className={styles.skill}/>
+            <FaPython className={styles.skill}/>
+            <GoRuby className={styles.skill}/>
+            <FaReact className={styles.skill}/>
+            <FaSwift className={styles.skill}/>
+            <FaHtml5 className={styles.skill}/>
+            <FaCss3 className={styles.skill}/>
+          </div>
         </div>
       </div>
     </section>
